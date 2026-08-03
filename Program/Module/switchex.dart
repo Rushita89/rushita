1d0fea07f701e16aaa4f3a91546bdc9a3e5086ed
Rushita89 <rushitadevmurari77@gmail.com>
2026-08-03 10:28:17 +0530

    Add files via upload

---
+++ Program/Module/switchex.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter Your Choice : \n Press 1 for English \n Press 2 for Hindi \n Press 3 for Gujrati");
+  var num=int.parse(stdin.readLineSync().toString());
+
+  switch(num)
+  {
+    case 1 : print("English");
+
+    case 2 : print("Hindi");
+
+    case 3 : print("Gujrati");
+  }
+}
