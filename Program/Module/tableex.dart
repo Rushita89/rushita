82707a41b0c27145ba74aa2f443f231807c86916
Rushita89 <rushitadevmurari77@gmail.com>
2026-08-05 10:26:08 +0530

    Add files via upload

---
+++ Program/Module/tableex.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Enter Value:");
+  var num=int.parse(stdin.readLineSync().toString());
+
+  for (int i = 1; i <= 10; i++) {
+    var ans = num * i;
+    print("$num*$i=$ans");
+  }
+}
