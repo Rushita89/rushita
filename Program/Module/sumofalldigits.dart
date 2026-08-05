c8abbc4dc5e72a5c95d3722d5ea80ca4b1672d2e
Rushita89 <rushitadevmurari77@gmail.com>
2026-08-05 10:25:30 +0530

    Add files via upload

---
+++ Program/Module/sumofalldigits.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main()
+{
+  print("Enter Any Number:");
+  var num=int.parse(stdin.readLineSync().toString());
+
+  var sum=0;
+  while(num>0)
+  {
+      int rem=num%10;
+      sum+=rem;
+      num=num~/10;
+  }
+  print(sum);
+}
