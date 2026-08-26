39446ebd420df7175f881306d43fa87afd203a92
Rushita89 <rushitadevmurari77@gmail.com>
2026-08-26 10:58:18 +0530

    Add files via upload

---
+++ Program/Module/setex.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  Set set = Set();
+  set.add("a");
+  set.add("b");
+  set.add("c");
+  set.add("d");
+  set.add("a");
+  set.add("p");
+
+  Set set2 = Set();
+  set2.add("p");
+  set2.add("q");
+  set2.add("r");
+  set2.add("s");
+
+  set.addAll(set2);
+  print(set);
+}
