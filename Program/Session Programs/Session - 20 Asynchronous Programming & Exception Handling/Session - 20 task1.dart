c934c5be676f8f27852f25f8c0d134f55d5d1ac6
Rushita89 <rushitadevmurari77@gmail.com>
2026-08-31 20:11:43 +0530

    Add files via upload

---
+++ Program/Session Programs/Session - 20 Asynchronous Programming & Exception Handling/Session - 20 task1.dart
@@ -0,0 +1,16 @@
+Future fetchTrendingSongs(int seconds, String msg)
+{
+  final duration = Duration(seconds: seconds);
+  return Future.delayed(duration).then((value) => msg);
+}
+
+main() async
+{
+  print("Loading Songs...");
+
+  await fetchTrendingSongs(2, "Trending songs fetched!").then((status) {
+    print(status);
+  });
+
+  print("Top songs loaded!");
+}
---
+++ Program/Session Programs/Session - 20 Asynchronous Programming & Exception Handling/Session - 20 task2.dart
@@ -0,0 +1,16 @@
+Future getOrderStatus(int seconds, String msg)
+{
+  final duration = Duration(seconds: seconds);
+  return Future.delayed(duration).then((value) => msg);
+}
+
+main() async
+{
+  print("Checking Order...");
+
+  await getOrderStatus(3, "Order Delivered").then((status) {
+    print(status);
+  });
+
+  print("Thank you for ordering with Zomato!");
+}
